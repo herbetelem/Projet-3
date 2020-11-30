d2f45dd9a75234ef4079eca799ce31596e1ec516
LauraBusignies <67093562+LauraBusignies@users.noreply.github.com>
2020-11-30 08:43:17 +0100

    login

--- website/all4animals/mysite/models.py
+++ website/all4animals/mysite/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 # Create your models here.
 
-# class User(models.Model):
+class User(models.Model):
 
-#     pseudo = models.CharField(max_length=100)
-#     email = models.EmailField(max_length=70)
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     address = models.CharField(max_length=400)
-#     postal_code = models.IntegerField() 
-#     host = models.BooleanField() 
+    pseudo = models.CharField(max_length=100)
+    email = models.EmailField(max_length=70)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=400)
+    postal_code = models.IntegerField() 
+    host = models.BooleanField() 
 
-# class Type_animal(models.Model):
+class Type_animal(models.Model):
 
-#     type = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
 
     
-# class Housing_planning(models.Model):
+class Housing_planning(models.Model):
 
-#     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     id_type_animal = models.ForeignKey(Type_animal, on_delete=models.CASCADE)
-#     number = models.IntegerField() 
-#     start_date = models.DateField()
-#     end_date = models.DateField()
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_type_animal = models.ForeignKey(Type_animal, on_delete=models.CASCADE)
+    number = models.IntegerField() 
+    start_date = models.DateField()
+    end_date = models.DateField()
 
 
