97457b9639eba0e8d0d5c4bceea5136c52856aeb
LauraBusignies <67093562+LauraBusignies@users.noreply.github.com>
2021-01-14 12:04:08 +0100

    [WIP] create post

--- website/all4animals/alert/urls.py
+++ website/all4animals/alert/urls.py
@@ -6,7 +6,7 @@ app_name = "alert"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('alerte/', views.alert, name='alerte'),
     path('annonce/', views.Alert_view.as_view(), name='annonce'),
     path('annonce/<int:pk>/', views.Alert_detail.as_view(), name='choix_alert'),
+    path('alerte/', views.alert, name='alerte'),
 ]
